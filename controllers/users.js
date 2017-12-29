d539533c3bf06c252e5538dbdaf17a9f6f61f1ae
Scott <ssanford@mediafly.com>
2017-12-29 13:23:33 -0600

    create signup route

--- controllers/users.js
+++ controllers/users.js
@@ -2,10 +2,16 @@ module.exports = function(_) {
     return {
         SetRouting: function(router) {
             router.get('/', this.indexPage)
+            router.get('/signup', this.getSignUp)
         }, 
 
         indexPage: function(req, res) {
             return res.render('index', {test: 'This is a test'})
+        }, 
+
+        getSignUp: function(req, res) {
+            return res.render('signup')
         }
+
     }
 }
