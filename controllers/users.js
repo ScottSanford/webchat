15c0e31d2032a22fd41a6941beabfe66a3da0fed
Scott <ssanford@mediafly.com>
2017-12-29 14:40:00 -0600

    create home route

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,16 @@
-module.exports = function(_) {
+const passport = require('passport')
+
+module.exports = function() {
     return {
         SetRouting: function(router) {
+            // GET Routes
             router.get('/', this.indexPage)
             router.get('/signup', this.getSignUp)
+            router.get('/home', this.homePage)
+
+
+            // POST Routes
+            router.post('/signup', this.postSignUp)
         }, 
 
         indexPage: function(req, res) {
@@ -11,6 +19,16 @@ module.exports = function(_) {
 
         getSignUp: function(req, res) {
             return res.render('signup')
+        }, 
+
+        postSignUp: passport.authenticate('local.signup', {
+            successRedirect: '/home', 
+            failureRedirect: '/signup', 
+            failureFlash: true
+        }), 
+
+        homePage: function (req, res) {
+            return res.render('home')
         }
 
     }
