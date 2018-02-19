37ee8f914a68f29d7be59e58db7b7069b91fb1a8
Scott <ssanford@mediafly.com>
2018-02-19 15:41:33 -0600

    Sender Document Update

--- controllers/group.js
+++ controllers/group.js
@@ -5,7 +5,7 @@ module.exports = function() {
     return {
         SetRouting: function(router) {
             router.get('/group/:name', this.groupPage)
-            router.get('/group/:name', this.groupPostPage)
+            router.post('/group/:name', this.groupPostPage)
         },
 
         groupPage: function(req, res) {
@@ -19,6 +19,7 @@ module.exports = function() {
 
         groupPostPage: function(req, res) {
             async.parallel([
+                // Update the Receiver List
                 function(callback) {
                     if(req.body.receiverName) {
                         // For the receiver of the friend request
@@ -27,17 +28,39 @@ module.exports = function() {
                             'request.userId': {$ne: req.user._id},
                             'friendsList': {$ne: req.user._id}
                         }, {
-                            $push: {request: {
-                                userId: req.user._id,
-                                username: req.user.username
-                            }},
+                            $push: {
+                                request: {
+                                    userId: req.user._id,
+                                    username: req.user.username
+                                }
+                            },
                             $inc: { totalRequest: 1}
                         }, (err, count) => {
                             callback(err, count)
                         })
                     }
+                },
+
+                // Update the Sender
+                function(callback) {
+                    if(req.body.receiverName) {
+                        Users.update({
+                            'username': req.user.username,
+                            'sentRequest.username': {$ne: req.body.receiverName}
+                        },{
+                            $push: {
+                                sentRequest: {
+                                    username: req.body.receiverName
+                                }
+                            }
+                        }, (err, count) => {
+                            callback(err, count)
+                        })
+                    }
                 }
-            ])
+            ], (err, results) => {
+                res.redirect(`/group/${req.params.name}`)
+            })
         }
     }
 }
