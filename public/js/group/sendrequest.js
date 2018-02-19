c36cd22df1221403323c6cf2169e69fa4108a29f
Scott <ssanford@mediafly.com>
2018-02-19 15:10:41 -0600

    listen for friend request event

--- public/js/group/sendrequest.js
+++ public/js/group/sendrequest.js
@@ -1,4 +1,42 @@
 $(document).ready(function(){
     var socket = io();
+
+    var room = $('#groupName').val();
+    var sender = $('#sender').val();
     
+    socket.on('connect', function(){
+        var params = {
+            sender: sender
+        };
+
+        socket.emit('joinRequest', params, function(){
+            console.log('Joined!')
+        })
+    });
+
+    socket.on('newFriendRequest', function(friend){
+        console.log(friend)
+    })
+
+    $('#add_friend').on('submit', function(e){
+        e.preventDefault();
+
+        var receiverName = $('#receiverName').val();
+
+        $.ajax({
+            url: '/group/' + room,
+            type: 'POST',
+            data: {receiverName: receiverName},
+            success: function() {
+                // this makes it real time
+                socket.emit('friendRequest', {
+                    receiver: receiverName,
+                    sender: sender
+                }, function(){
+                    console.log('Request sent')
+                });
+            }
+        });
+    })
+
 });
