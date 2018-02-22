3fcb382515ca96a6ca5cf371bb9223376e60d7ac
Scott <ssanford@mediafly.com>
2018-02-22 15:42:04 -0600

    emit global users

--- socket/globalroom.js
+++ socket/globalroom.js
@@ -1,9 +1,18 @@
+const _ = require('lodash')
+
 module.exports = function(io, Global) {
-    const users = new Global()
+    const clients = new Global()
 
     io.on('connection', socket => {
         socket.on('global room', global => {
             socket.join(global.room)
+
+            clients.EnterRoom(socket.id, global.name, global.room, global.img)
+
+            const nameProp = clients.GetRoomList(global.room)
+            const array = _.uniqBy(nameProp, 'name')
+
+            io.to(global.room).emit('loggedInUser', array)
         })
     })
 }
