6b65ca8712d97728aa511092d6e530e1e42591e6
Scott <ssanford@mediafly.com>
2018-03-14 16:57:18 -0500

    emit private chat join event

--- public/js/privatechat/pm.js
+++ public/js/privatechat/pm.js
@@ -6,7 +6,33 @@ $(document).ready(function(){
     
     swap(newParam, 0, 1)
     
-    var paramTwo = newParam[0] + '.' + newParam[1]
+    var paramTwo = newParam[0] + '.' + newParam[1];
+
+    socket.on('connect', function() {
+        var params = {
+            roo1: paramOne,
+            roo2: paramTwo,
+        }
+
+        socket.emit('join PM', params);
+    })
+
+    // Sending event 'createMessage' to the server
+    $('#message_form').on('submit', function (e) {
+        e.preventDefault();
+
+        var message = $('#msg').val();
+        var sender = $('#name-user').val();
+
+        if (message.trim().length > 0) {
+            socket.emit('private message', {
+                text: message,
+                from: sender
+            }, function () {
+                $('#msg').val('');
+            });
+        }
+    })
 });
 
 function swap(input, value1, value2) {
--- socket/pm.js
+++ socket/pm.js
@@ -1,5 +1,13 @@
 module.exports = function(io) {
     io.on('connection', socket => {
-        
+        socket.on('join PM', pm => {
+            socket.join(pm.room1)
+            socket.join(pm.room2)
+
+        })
+
+        socket.on('private message', message => {
+            console.log(message)
+        })
     })
 }
